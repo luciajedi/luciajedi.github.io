dbfee48ff24cc32308ddcf28c99fd86ab016024b
Lucia <36191350+luciajedi@users.noreply.github.com>
2019-02-04 13:13:30 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 window.onload = function(){
-	document.getElementById("container").style.display="flex";
 	setTimeout(function(){
 	document.getElementById("container").style.display="none";
 }, 3000);
