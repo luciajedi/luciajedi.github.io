98a9d53b0e42bf753624f5575839504d8230c1fd
Lucia <36191350+luciajedi@users.noreply.github.com>
2019-02-04 12:55:21 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+	document.getElementById("container").style.display="flex";
+	setTimeout(function(){
+	document.getElementById("container").style.display="none";
+}, 3000);
+}
